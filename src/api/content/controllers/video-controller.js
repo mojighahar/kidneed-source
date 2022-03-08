0ebe82fd3dad0442c583b7805303fd4fb8328224
ali shabani <alishabani9270@gmail.com>
2022-03-08 19:34:03 +0330

    feat: add videos route

---
+++ src/api/content/controllers/video-controller.js
@@ -0,0 +1,22 @@
+"use strict";
+module.exports = {
+  async index(ctx) {
+    const videos = await strapi.query("api::content.content").findMany({
+      where: { type: "video" },
+      select: ["id", "uuid", "title", "meta"],
+    });
+
+    const result = videos.map((v) => {
+      const duration = v.meta?.duration;
+
+      return {
+        id: v.id,
+        uuid: v.uuid,
+        title: v.title,
+        duration,
+      };
+    });
+
+    return { result };
+  },
+};
