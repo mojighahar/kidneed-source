58a728de0d0f90f4fe407b4a9db93a64f3dd64fd
Ali Shabani <alishabani9270@gmail.com>
2022-03-09 21:35:47 +0330

    fix tags api

--- src/api/tag/controllers/tag.js
+++ src/api/tag/controllers/tag.js
@@ -1,9 +1,13 @@
-'use strict';
+"use strict";
 
 /**
  *  tag controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::tag.tag');
+module.exports = createCoreController("api::tag.tag", ({ strapi }) => ({
+  async find(ctx) {
+    strapi.db.query("api::tag.tag").findAll();
+  },
+}));
