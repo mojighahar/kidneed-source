f046277d5b9591a2ea2280f96477db32c25b6a89
Hamid Sarani <h.sarani75@gmail.com>
2022-03-09 21:47:37 +0330

    fix tags

--- src/api/tag/controllers/tag.js
+++ src/api/tag/controllers/tag.js
@@ -8,6 +8,6 @@ const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::tag.tag", ({ strapi }) => ({
   async find(ctx) {
-    strapi.db.query("api::tag.tag").findAll();
+    strapi.db.query("api::tag.tag").find();
   },
 }));
