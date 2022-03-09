4194841867126ab35264c63f3b46ecd099af999d
Hamid Sarani <h.sarani75@gmail.com>
2022-03-09 21:56:10 +0330

    fix tags

--- src/api/tag/controllers/tag.js
+++ src/api/tag/controllers/tag.js
@@ -6,8 +6,8 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::tag.tag", ({ strapi }) => ({
+module.exports = createCoreController("api::tag.tag", () => ({
   async find(ctx) {
-    strapi.db.query("api::tag.tag").find();
+    strapi.query("api::tag.tag").findMany();
   },
 }));
