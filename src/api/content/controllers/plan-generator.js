25712b83a8b9a7c4bfa5cd5dcb3f2e94baeac1d1
Mojtaba Ghahari <mojighahar@gmail.com>
2022-03-10 18:27:39 +0330

    fix generator

--- src/api/content/controllers/plan-generator.js
+++ src/api/content/controllers/plan-generator.js
@@ -24,7 +24,6 @@ module.exports = {
       audio: await fetchData("audio", Math.floor((daysCount / 3) * 3)),
       game: await fetchData("game", Math.floor((daysCount / 3) * 3)),
     };
-    1;
 
     const plan = [];
     for (let i = 0; i < daysCount; i++) {
@@ -34,22 +33,22 @@ module.exports = {
         contents: [
           {
             type: "video",
-            duration: contents.video[i * 2].meta.duration,
+            duration: contents.video[i * 2].meta?.duration,
             content1Id: contents.video[i * 2].id,
             content2Id: contents.video[i * 2 + 1].id,
           },
           {
             type: "activity",
             duration: 0,
-            content1Id: contents.activity[i * 2].id,
-            content2Id: contents.activity[i * 2 + 1].id,
+            content1Id: contents.activity[i * 2]?.id,
+            content2Id: contents.activity[i * 2 + 1]?.id,
           },
           {
             type: contentType,
             duration:
               contentType == "audio"
                 ? contents[contentType][Math.floor(i / 3) * 2]?.chapter.reduce(
-                    (sum, item) => sum + item.duration,
+                    (sum, item) => sum + item?.duration,
                     0
                   )
                 : 0,
